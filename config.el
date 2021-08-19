b5aabcc2242fc383dd34ed20d10eefcadfd309e1
Cheng Zhao <zhcheng127@gmail.com>
2021-08-19 23:25:08 +0800

    梳理图计算分类

--- config.el
+++ config.el
@@ -26,7 +26,7 @@
 ;; available. You can either set `doom-theme' or manually load a theme with the
 ;; `load-theme' function. This is the default:
 ;;
-(setq doom-theme 'doom-one-light)
+(setq doom-theme 'doom-monokai-pro)
 ;;(setq doom-theme 'doom-city-lights)
 
 ;; If you use `org' and don't want your org files in the default location below,
@@ -57,16 +57,16 @@
 ;; You can also try 'gd' (or 'C-c c d') to jump to their definition and see how
 ;; they are implemented.
 
-(use-package! org-roam
-  :commands (org-roam-insert org-roam-find-file org-roam)
-  :init
-  (setq org-roam-directory "~/org/org-roam/")
-  (setq org-roam-graph-viewer "/usr/bin/open")
-  (map! :leader
-  :prefix "r"
-  :desc "Org-Roam-Insert" "i" #'org-roam-insert
-  :desc "Org-Roam-Find"   "/" #'org-roam-find-file
-  :desc "Org-Roam-Buffer" "r" #'org-roam)
-  :config
-  (org-roam-mode +1))
-(add-hook 'after-init-hook 'org-roam-mode)
+;;(use-package! org-roam
+;;  :commands (org-roam-insert org-roam-find-file org-roam)
+;;  :init
+;;  (setq org-roam-directory "~/org/org-roam/")
+;;  (setq org-roam-graph-viewer "/usr/bin/open")
+;;  (map! :leader
+;;  :prefix "r"
+;;  :desc "Org-Roam-Insert" "i" #'org-roam-insert
+;;  :desc "Org-Roam-Find"   "/" #'org-roam-find-file
+;;  :desc "Org-Roam-Buffer" "r" #'org-roam)
+;;  :config
+;;  (org-roam-mode +1))
+;;(add-hook 'after-init-hook 'org-roam-mode)
--- custom.el
+++ custom.el
@@ -3,10 +3,8 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   '(fcitx exec-path-from-shell websocket org-roam-bibtex org-roam-server)))
  '(custom-safe-themes
-   '("cebda623e3f9ecd4a4af08d92c406bc4517e323d9259d5156f43901860224e30" "22dadcc3e29840ef054c188b5cfe88c71c3bf77afc58d1c4ff9ab5dd3ef60e9b" default))
+   '("b0e446b48d03c5053af28908168262c3e5335dcad3317215d9fdeb8bac5bacf9" "cebda623e3f9ecd4a4af08d92c406bc4517e323d9259d5156f43901860224e30" "22dadcc3e29840ef054c188b5cfe88c71c3bf77afc58d1c4ff9ab5dd3ef60e9b" default))
  '(package-selected-packages '(zzz-to-char)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
