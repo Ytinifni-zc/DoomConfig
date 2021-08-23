c17a9020e2b489bfe63312385b7f799ff8c6adbe
Ytinifni-zc <zhcheng127@gmail.com>
2021-08-23 17:30:27 +0800

    Update orgs

--- custom.el
+++ custom.el
@@ -3,11 +3,10 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   '(fcitx exec-path-from-shell websocket org-roam-bibtex org-roam-server)))
  '(custom-safe-themes
    '("cebda623e3f9ecd4a4af08d92c406bc4517e323d9259d5156f43901860224e30" "22dadcc3e29840ef054c188b5cfe88c71c3bf77afc58d1c4ff9ab5dd3ef60e9b" default))
- '(package-selected-packages '(zzz-to-char)))
+ '(package-selected-packages
+   '(org-latex-impatient popper org-noter-pdftools org-pdftools org-noter proxy-mode pdf-tools zzz-to-char)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
